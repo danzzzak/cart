9c1bcb74a0811779bf5b20c120c1734e5b74a38d
danzzzak <siriuz2000@mail.ru>
2020-12-05 16:33:02 +0500

    Отображение продукта в корзине

--- scripts.js
+++ scripts.js
@@ -89,7 +89,10 @@ class UI {
                 // Достать данные продукта
                 this.setCartValues(cart);
                 // Отобразить продукт
+                this.addCartItem(cartItem);
                 // Показать корзину
+                this.showCart();
+
             });
         });
     }
@@ -102,7 +105,28 @@ class UI {
         });
         cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
         cartItems.innerText = itemsTotal;
-        console.log(cartTotal, cartItems);
+    }
+    addCartItem(item) {
+        const div = document.createElement('div');
+        div.classList.add('cart-item');
+        div.innerHTML = `
+        <img src=${item.image} alt="1" class="">
+        <div>
+            <h4>${item.title}</h4>
+            <h5>${item.price}</h5>
+            <span class="remove-item" data-id=${item.id}>remove</span>
+        </div>
+        <div>
+            <i class="fas fa-chevron-up" data-id=${item.id}></i>
+            <p class="item-amount">${item.amount}</p>
+            <i class="fas fa-chevron-down" data-id=${item.id}></i>
+        </div>
+        `;
+        cartContent.appendChild(div);
+    }
+    showCart() {
+        cartOverlay.classList.add('transparentBcg');
+        cartDOM.classList.add('showCart');
     }
 }
 class Storage {
